e4447c919f0d08409f0808ac31402f95c6672c4f
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-06-05 12:10:34 -0700

    init confit BATCH_SIZE default is a number instead of string

--- src/config/init.config.js
+++ src/config/init.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongo_uri: process.env.MONGO_URI,
     GMAIL_BATCH_ENDPOINT: process.env.GMAIL_BATCH_ENDPOINT || 'https://www.googleapis.com/batch/gmail/v1',
-    ACTIONS_BATCH_SIZE: process.env.ACTIONS_BATCH_SIZE || '50',
+    ACTIONS_BATCH_SIZE: process.env.ACTIONS_BATCH_SIZE || 50,
     log_level: process.env.LOG_LEVEL || 'production',
     GMAIL_LABEL_ENDPOINT: process.env.GMAIL_LABEL_ENDPOINT || 'https://www.googleapis.com/gmail/v1/users/me/labels',
     GAPI_INIT_RETRY_DELAY: Number(process.env.GAPI_INIT_RETRY_DELAY) || 500,
