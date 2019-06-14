f72bc36070e573c1bd0540793c02e952636fd2d3
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-06-13 19:16:29 -0700

    added snippet to thread_id model

--- src/core/models/Thread_ID.model.js
+++ src/core/models/Thread_ID.model.js
@@ -7,7 +7,8 @@ const Schema = mongoose.Schema;
 const threadIdSchema = new Schema({
   userId: { type: String, required: true },
   threadId: { type: String, required: true },
-  senderId: { type: String, require: false }
+  senderId: { type: String, require: false },
+  snippet: { type: String, required: false }
 });
 
 const ThreadId = mongoose.model('Thread-Ids', threadIdSchema);
