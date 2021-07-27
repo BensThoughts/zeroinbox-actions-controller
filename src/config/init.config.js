d3114f7ca3323a7ffcce3e673a3933bd7d6e4f92
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-07-27 14:38:25 -0700

    fixed default endpoint in config

--- src/config/init.config.js
+++ src/config/init.config.js
@@ -4,7 +4,7 @@ module.exports = {
     ACTIONS_HEALTH_PORT: Number(process.env.ACTIONS_HEALTH_PORT),
     GMAIL_BATCH_ENDPOINT: String(process.env.GMAIL_BATCH_ENDPOINT || 'https://www.googleapis.com/batch/gmail/v1'),
     GMAIL_BATCH_MODIFY_SIZE: Number(process.env.GMAIL_BATCH_MODIFY_SIZE || 1000),
-    GMAIL_BATCH_MODIFY_ENDPOINT: String(process.env.GMAIL_BATCH_MODIFY_ENDPOINT || 'gmail/v1/users/me/messages/batchModify'),
+    GMAIL_BATCH_MODIFY_ENDPOINT: String(process.env.GMAIL_BATCH_MODIFY_ENDPOINT || 'https://gmail.googleapis.com/gmail/v1/users/me/messages/batchModify'),
     BATCHELOR_BATCH_SIZE: Number(process.env.BATCHELOR_BATCH_SIZE || 1),
     LOG_LEVEL: String(process.env.LOG_LEVEL || 'production'),
     GMAIL_LABEL_ENDPOINT: String(process.env.GMAIL_LABEL_ENDPOINT || 'https://www.googleapis.com/gmail/v1/users/me/labels'),
